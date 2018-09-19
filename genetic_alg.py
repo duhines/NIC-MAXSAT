b12f24a5b7aca8a4583fb10625b9fa57d8dd8e88
Dustin Hines <dhines@dhcp-222-254.wireless.bowdoin.edu>
2018-09-19 11:29:26 -0400

    working on parsing

---
+++ genetic_alg.py
@@ -0,0 +1,15 @@
+"""
+1. Get the parser going.
+	i. read cnf file->determine number of literals for the size of the individuals representation 
+	ii. put the file in an array, where each item in array is one clause from the file (one line)
+		a. clause ex:  ..., 8, 3, -4,... (negative values indicate that the literal is negated) 
+	iii. save the boolean values as strings
+
+
+
+
+"""
+
+def main()
+	
+	file = ""
