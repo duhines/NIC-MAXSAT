50b6407ca72d7fc67040434a3a653104035ec841
Dustin Hines <dhines@dhcp-223-202.wireless.bowdoin.edu>
2018-09-21 00:53:51 -0400

    did some stuff: dunc + dustin

--- genetic_alg.py
+++ genetic_alg.py
@@ -9,7 +9,178 @@
 
 
 """
+"""
+    STYLE: 
+        - 80 chars max per line
+        - lets use underscores instead of camel case
+        - function docstrings inside functions
+        - module docstring at top of each file with 
+            - authors, purpose, bugs, etc.
+"""
+import parse_input as parse
+import sys
+import random
 
-def main()
+
+file = "maxsat-problems/maxsat-crafted/MAXCUT/SPINGLASS/t3pm3-5555.spn.cnf"
+def generate_initial_pop(problem, pop_size):
+	"""
+	Purpose:
+	Input: 
+	Return:
+	"""
+	population = []
+	individual = []
+	for i in range(0, pop_size):
+		for j in range(0, problem["num_literals"]):
+			individual.append(random.random() > .5)
+		population.append(individual.copy())
+		del individual[:]
+
+	return population
+
+
+def fitness(individual, problem):
+	"""
+	Purpose:
+	Input: 
+	Return:
+	"""
+	fitness = 0
+	for clause in problem["clauses"]:
+		check = False
+		for literal in clause:
+			if literal > 0:
+				check = check or individual[literal - 1]
+			else:
+				check = check or not individual[abs(literal) - 1]
+		if check:
+			fitness += 1
+	return fitness
+
+
+
+
+def rank_select(scored_generation):
+	#thanks: https://stackoverflow.com/questions/3121979/how-to-sort-list-tuple-of-lists-tuples
+	sorted_generation = sorted(scored_generation, key=lambda individual: individual[1])
+	total_rank = 0
+	for i in range(1, len(sorted_generation) + 1):
+		total_rank += i 
+
+	selected_individuals = []
+	for i in range(0, len(sorted_generation)):
+		base = 0
+		increment = 1
+		rand_value = random.randint(0, total_rank - 1)
+		while rand_value > base:
+			increment += 1
+			base += increment
+		selected = sorted_generation[increment - 1][0]
+		selected_individuals.append(selected)
+
+	print(selected_individuals)
+	return selected_individuals
+
+
+def tournament_select():
+	return
+
+
+def boltzmann_select():
+	return
+
+
+def select(scored_generation, selection_method):
+	if selection_method == "rs":
+		return rank_select(scored_generation)
+	elif selection_method == "ts":
+		return tournament_select(scored_generation)
+	elif selection_method == "bs":
+		return boltzmann_select(scored_generation)
+
+def recombination(selected, prob, type):
+	next_gen = []
+	for i in range (0, len(selected)//2):
+		first = selected[random.randint(0, len(selected) - 1)]
+		second = selected[random.randint(0, len(selected) - 1)]
+		if random.random() < prob:
+			if type == "1c":
+				offspring = single_crossover(first, second)
+			else:
+				offspring = uniform_crossover(first, second)
+			next_gen.append(offspring[0])
+			next_gen.append(offspring[1]) 
+		else:
+			next_gen.append(first)
+			next_gen.append(second)
+	return next_gen
+
+
+def single_crossover(individual1, individual2):
+	print("crossover\n")
+	print(individual1)
+	print(individual2)
+	crossover_point = random.randint(1, len(individual1)-2)
+	print(crossover_point)
+	first_child = individual1[:crossover_point].copy() + individual2[crossover_point:].copy()
+	second_child = individual2[:crossover_point].copy() + individual1[crossover_point:].copy()
+	print(first_child)
+	print(second_child)
+	return (first_child, second_child)
+
+def uniform_crossover(individual1, individual2):
+	return
+
+def mutate(population):
+	return
+
+
+def standard_GA(problem, parameters):
+	initial_generation = generate_initial_pop(problem, parameters["pop_size"])
+
+	for individual in initial_generation:
+		print(individual)
+		print(fitness(individual, problem))
+	iteration = 0
+	current_generation = initial_generation.copy()
+	while iteration < parameters["num_generations"]:
+		iteration += 1
+		scored_generation = []
+		for individual in current_generation:
+			score = fitness(individual, problem)
+			scored_generation.append((individual, score))
+		selected = select(scored_generation, parameters["selection_type"])
+		recombinated_generation = recombination(selected, parameters["crossover_prob"], parameters["crossover_method"])
+	return
+
+
+def main():
+	#aquire command line arguments
+	parameters = {
+		"file_name": sys.argv[1],
+		"pop_size": int(sys.argv[2]),
+		"selection_type": sys.argv[3],
+		"crossover_method": sys.argv[4],
+		"crossover_prob": float(sys.argv[5]),
+		"mutation_prob": float(sys.argv[6]),
+		"num_generations": int(sys.argv[7]),
+		"algorithm": sys.argv[2]
+	}
+	if parameters["pop_size"] % 2 != 0:
+		parameters["pop_size"] += 1
+	problem = [[1, -4], [-2, -3], [4, 1], [-4, 4], [-3, 1], [-1, 2], [1, 1], [-1, -1]]
+	sample_problem = {
+    	"num_literals": 4,
+    	"num_clauses": 8,
+    	"clauses": problem
+    }
+	#aquire MAXSAT problem
+	problem = parse.return_problem(file)
 	
-	file = ""
+	solution = standard_GA(sample_problem, parameters)
+
+	print(problem["num_literals"])
+	
+
+main()
--- parse_input.py
+++ parse_input.py
@@ -1,55 +1,90 @@
 '''
-General things to keep in mind
-* Individuals are represented as arrays of literals with a binary 0 or 1 (F or T) value. For example, an individual
-in a problem with 3 literals might look like [0, 0, 1] where x0 = F, x1 = F, x2 = T.
-* Clauses are represented as arrays indices specifying which indexes in an individual to test. For example, clause
-[1, 0] would translate as 'x1 | x0' and would be false for the example individual above.
+General things to keep in mind:
+* Individuals are represented as arrays of literals with a binary 0 or 1 
+    (F or T) value. For example, an individual in a problem with 3 literals 
+    might look like [0, 0, 1] where x0 = F, x1 = F, x2 = T.
+* Clauses are represented as arrays indices specifying which indexes in an
+    individual to test. For example, clause [1, 0] would translate as 'x1 | x0'
+    and would be false for the example individual above.
 '''
 
-# ATTN DUNC AND DUSTIN: I'm doing this in an IDE on a PC, so chances are you'll have to change this line to accommodate
-# whatever your local file format is. ofc we'll eventually allow the filename to specified as input, but for now this is
-# how it is.
-test_file = r"maxsat-problems/maxsat-crafted/MAXCUT/DIMACS_MOD/brock200_1.clq.cnf"
+# ATTN DUNC AND DUSTIN: I'm doing this in an IDE on a PC, so chances are you'll
+#   have to change this line to accommodate whatever your local file format is. 
+#   ofc we'll eventually allow the filename to specified as input, but for now 
+#   this is how it is.
+# NOTE: this should stay the same as long as we're all working from a clone of 
+#   the repo!
 
-# Initialize the variables we're going to need to keep track of the length of our array of literals (vars x1,...,xn)
-# and Clauses (TF statements that we're trying to MAXSAT), Technically you don't have to initialize anything in Python,
-# but I like the structure it lends the program.
-numLiterals = 0  # To be set upon file Parsing
-numClauses = 0  # To be set upon file Parsing
-clauseList = [] # Initializing a variable
-'''
-Function to define how many clauses and literals we are dealing with.
-Format: F(str) -> [numLits, numClauses]
-'''
+
+"""
+    STYLE: 
+        - 80 chars max per line
+        - lets use underscores instead of camel case
+        - function docstrings inside functions
+        - module docstring at top of each file with 
+            - authors, purpose, bugs, etc.
+"""
+
+#Initialize the variables we're going to need to keep track of the length of
+#   our array of literals (vars x1,...,xn) and Clauses (TF statements that 
+#   we're trying to MAXSAT), Technically you don't have to initialize anything
+#   in Python, but I like the structure it lends the program.
+
+num_literals = 0  # To be set upon file Parsing
+num_clauses = 0  # To be set upon file Parsing
+clause_list = [] # Initializing a variable
+
 def get_num_clauses(filename):
+    '''
+    Function to define how many clauses and literals we are dealing with.
+    Format: F(str) -> [numLits, num_clauses]
+    '''
     with open(filename, 'r+') as cnf_file:
-        for l in cnf_file.readlines():
-            if l.startswith('p'):  # Lines starting with P define the number of literals and clauses we are dealing with
-                return l.split()[2:4]
+        for line in cnf_file.readlines():
+            # Lines starting with P define the number of literals and 
+            #   clauses we are dealing with
+            if line.startswith('p'):  
+                return line.split()[2:4]
 
 
-'''
-Take a cnf file and return an list containing all the clauses from the file. The clause array is formatted as an array
-of arrays, with each subarray being a clause. You can think of each sub array as consisting of N truth functional vars
-represented as indices for our literals list. For example, subarray [0, 1, 2] would translate as 'x0 | x1 | x2' whereas
-[0, 1, -2] would translate as 'x0 & x1 & ~x2'. This translation will happen when 
-'''
+
 def make_array_of_clauses(filename):
+    '''
+    Take a cnf file and return an list containing all the clauses from the 
+    file. The clause array is formatted as an array of arrays, with each 
+    subarray being a clause. You can think of each sub array as consisting
+    of N truth functional vars represented as indices for our literals list.
+    For example, subarray [0, 1, 2] would translate as 'x0 | x1 | x2' whereas
+    [0, 1, -2] would translate as 'x0 & x1 & ~x2'. This translation will happen
+    when 
+    '''
     with open(filename, 'r+') as cnf_file:
         clauses = []
-        for l in cnf_file.readlines():
-            if not l[0].isalpha():  # Lines that don't start with c or p contain a clause:
-                clauses.append(l.strip().split())
+        for line in cnf_file.readlines():
+            # Lines that don't start with c or p contain a clause:
+            if not line[0].isalpha(): 
+                clause_as_strings = line.replace("0", "").strip().split()
+                clause_as_ints = []
+                for literal in clause_as_strings:
+                    clause_as_ints.append(int(literal)) 
+                clauses.append(clause_as_ints)
+        print(clauses)
         return clauses
 
 
-def main():
-    litAndClauseNums = get_num_clauses(test_file)
-    numLiterals = litAndClauseNums[0]
-    numClauses = litAndClauseNums[1]
-    print(numLiterals, numClauses)
-    clauseList = make_array_of_clauses(test_file)
-    print(clauseList)
-
-
-main()
+def return_problem(file_name):
+    """
+    Purpose:
+    Parameters:
+    Return:
+    """
+    lit_and_clause_nums = get_num_clauses(file_name)
+    num_literals = lit_and_clause_nums[0]
+    num_clauses = lit_and_clause_nums[1]
+    clause_list = make_array_of_clauses(file_name)
+    problem = {
+        "num_literals": int(num_literals),
+        "num_clauses": int(num_clauses),
+        "clauses": clause_list
+    }
+    return problem
